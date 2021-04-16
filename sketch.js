94aeae79baead7e51d30c6b46bbdf6f3bc4d1014
PranavBahrani <69100517+PranavBahrani@users.noreply.github.com>
2021-04-16 21:48:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+ var hr = hour();
+ var mn = minute();
+ var sc = second();
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
